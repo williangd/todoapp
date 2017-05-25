103511de8e6de5afd3f0b0dfae217b4023ca2099
Willian Oliveira <williangd@yahoo.com.br>
2017-05-25 14:58:59 +0000

    Initial commit

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,34 @@
+class TodosController < ApplicationController
+  
+  def index
+    @todos = Todo.all
+  end
+  
+  
+  def new
+    @todo = Todo.new
+  end
+  
+  
+  def create
+    @todo = Todo.new(todo_params)
+    if @todo.save
+      flash[:notice] = "Todo successfully created"
+      redirect_to todo_path(@todo)
+    else
+      render 'new'
+    end
+  end
+  
+  
+  def show
+    @todo = Todo.find(params[:id])
+  end
+  
+  
+  private 
+  
+    def todo_params
+      params.require(:todo).permit(:name, :description)
+    end
+end
